bc24cadfb33167e99ad3f7159ca7bd013baeb15a
Jeff Willis <jwillis@myocv.com>
2017-02-16 16:26:41 -0600

    2 years later

--- app/src/main/java/com/tumblr/asixjin/asixsgreatendecoder/MainActivity.java
+++ app/src/main/java/com/tumblr/asixjin/asixsgreatendecoder/MainActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.EditText;
 import android.widget.Switch;
@@ -12,101 +15,136 @@ import android.widget.Switch;
 
 public class MainActivity extends ActionBarActivity {
 
+    private Button mSubmitButton;
+    private Switch mToogle;
+    private EditText mMessageField;
+    private EditText mShiftValue;
+    private TextView mResultMessage;
+
+    private boolean toEncode = true;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main);
+
+        mSubmitButton = (Button) findViewById(R.id.submitButton);
+        mToogle = (Switch) findViewById(R.id.eTog);
+        mMessageField = (EditText) findViewById(R.id.dialogg);
+        mShiftValue = (EditText) findViewById(R.id.encodeNumm);
+        mResultMessage = (TextView) findViewById(R.id.result);
+
+        mToogle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                toEncode = isChecked;
+                if(toEncode){
+                    mSubmitButton.setText("ENCODE");
+                }else{
+                    mSubmitButton.setText("DECODE");
+                }
+            }
+        });
+
+        mSubmitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String eMsg = mMessageField.getText().toString();
+                int code;
+
+                try{
+                    code = Integer.parseInt(mShiftValue.getText().toString());
+                }catch(NumberFormatException e){
+                    code = 0;
+                }
+
+                if(toEncode){
+                    mResultMessage.setText(encode(eMsg, code));
+                }else{
+                    mResultMessage.setText(decode(eMsg, code));
+                }
+            }
+        });
     }
 
+    public String encode(String text, int shiftValue) {
+        StringBuilder msg =  new StringBuilder(text);
+        if(isValidShift(shiftValue)) {
+            int encodeLength = msg.length() - 1;
+            for(int i=0; i <= encodeLength; i++)
+            {
+                if(msg.charAt(i) == ' '){
+                    continue;
+                }
+                int newCharIndex = Character.digit(msg.charAt(i), 36) + shiftValue;
+                if(newCharIndex >= 36)
+                {
+                    newCharIndex -= 36;
+                }
+                msg.setCharAt(i, Character.forDigit(newCharIndex, 36));
+            }
+            return msg.toString();
+        }else{
+            return "Invalid number! Please try again!";
+        }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public String decode(String text, int shiftValue) {
+        StringBuilder msg = new StringBuilder(text);
+        if(isValidShift(shiftValue)){
+            int encodeLength = msg.length() - 1;
+            for(int i=0; i <= encodeLength; i++)
+            {
+                if(msg.charAt(i) == ' '){
+                    continue;
+                }
+                int newCharIndex = Character.digit(msg.charAt(i), 36) - shiftValue;
+                if(newCharIndex < 0)
+                {
+                    newCharIndex += 36;
+                }
+                msg.setCharAt(i, Character.forDigit(newCharIndex, 36));
+            }
+            return msg.toString();
+        }else{
+            return "Invalid number! Please try again!";
+        }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+    }
+
+    public boolean isValidShift(int shiftValue){
+        if(shiftValue > 35 || shiftValue <= 0)
+        {
+            return false;
+        }else{
             return true;
         }
-
-        return super.onOptionsItemSelected(item);
     }
 
-    public void clicked(View v)
-    {
-        Switch toggle = (Switch) findViewById(R.id.eTog);
-        EditText message = (EditText) findViewById(R.id.dialogg);
-        EditText numCode = (EditText) findViewById(R.id.encodeNumm);
-        TextView endMsg = (TextView) findViewById(R.id.result);
-
-        StringBuilder eMsg = new StringBuilder(message.getText());
-        int code;
-
-        try
-        {
-           code = Integer.parseInt(numCode.getText().toString());
-        }
-        catch(NumberFormatException e)
-        {
-           code = 0;
-        }
-
-
-        if(toggle.isChecked())
-        {
-            endMsg.setText(decode(eMsg, code));
-        }
-        else
-        {
-            endMsg.setText(encode(eMsg, code));
-        }
-    }
-
-    public StringBuilder encode(StringBuilder msg, int shiftValue)
-    {
-        if(shiftValue > 35 || shiftValue <= 0)
-        {
-            return new StringBuilder("Invalid number! Please try again!");
-        }
-
-        int encodeLength = msg.length() - 1;
-        for(int i=0; i <= encodeLength; i++)
-        {
-            int newCharIndex = Character.digit(msg.charAt(i), 36) + shiftValue;
-            if(newCharIndex >= 36)
-            {
-                newCharIndex -= 36;
-            }
-            msg.setCharAt(i, Character.forDigit(newCharIndex, 36));
-        }
-        return msg;
-    }
-
-    public StringBuilder decode(StringBuilder msg, int shiftValue)
-    {
-        if(shiftValue > 35 || shiftValue <= 0)
-        {
-            return new StringBuilder("Invalid number! Please try again!");
-        }
-        int encodeLength = msg.length() - 1;
-        for(int i=0; i <= encodeLength; i++)
-        {
-            int newCharIndex = Character.digit(msg.charAt(i), 36) - shiftValue;
-            if(newCharIndex < 0)
-            {
-                newCharIndex += 36;
-            }
-            msg.setCharAt(i, Character.forDigit(newCharIndex, 36));
-        }
-        return msg;
-    }
+    //region Old Unneeded Menu Code I might want later
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
+    //endregion
 }
